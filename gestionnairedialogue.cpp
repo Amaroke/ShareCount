afda21fc179c8763647ca4530864bc65870bc3b1
iopeti2u <hugo.iopeti5@etu.univ-lorraine.fr>
2021-11-13 09:45:40 +0100

    Ajout de commentaires dans les classes gestionnairedialogue.cpp/.hpp.

--- gestionnairedialogue.cpp
+++ gestionnairedialogue.cpp
@@ -1,5 +1,11 @@
+/* gestionnairedialogue.cpp
+ * version 5
+ * H. Iopeti, univ. Lorraine
+ */
+
 #include "gestionnairedialogue.hpp"
 
+// Constructeur de la classe GestionnaireDialogue.
 GestionnaireDialogue::GestionnaireDialogue()
     : utilisateur_connecte("", 0, "", "")
 {
@@ -8,18 +14,22 @@ GestionnaireDialogue::GestionnaireDialogue()
     set_utilisateur_connecte(Utilisateur("a", 1, "aa", "aaa"));
 }
 
+//Destructeur de la classe GestionnaireDialogue.
 GestionnaireDialogue::~GestionnaireDialogue() { }
 
+//Fonction qui retourne un utilisateur connecté.
 Utilisateur GestionnaireDialogue::get_utilisateur_connecte()
 {
     return utilisateur_connecte;
 }
 
+//Fonction qui défini un utilisateur connecté.
 void GestionnaireDialogue::set_utilisateur_connecte(Utilisateur utilisateur)
 {
     utilisateur_connecte = utilisateur;
 }
 
+//Fonction qui recherche un utilisateur à l'aide de son mail et de son mot de passe.
 Utilisateur GestionnaireDialogue::recherche_utilisateur(std::string adresse_mail,
                                                         std::string mot_de_passe)
 {
--- gestionnairedialogue.hpp
+++ gestionnairedialogue.hpp
@@ -1,16 +1,38 @@
+/** @file gestionnairedialogue.hpp
+ **
+ ** @brief Le fichier qui contient la classe GestionnaireDialogue.
+ **
+ ** @version 5
+ **
+ ** @author H. Iopeti, univ. Lorraine
+ **/
+
 #ifndef GESTIONNAIREDIALOGUE_HPP
 #define GESTIONNAIREDIALOGUE_HPP
 #include "utilisateur.hpp"
 
+/** @brief La classe GestionnaireDialogue gère les fenêtres d'alertes.
+ **
+ ** Elle contient 1 méthode de construction, 1 méthode de destruction
+ ** et 3 fonctions.
+ **
+ ** @version 5
+ **
+ ** @author H. Iopeti, univ. Lorraine
+ **/
 class GestionnaireDialogue {
 private:
     Utilisateur utilisateur_connecte;
 
 public:
     GestionnaireDialogue();
+    /// @brief Destructeur de la classe, ne fait rien.
     ~GestionnaireDialogue();
+    /// @brief Fonction qui retourne l'utilisateur connecté.
     Utilisateur get_utilisateur_connecte();
+    /// @brief Fonction qui définit un utilisateur connecté.
     void set_utilisateur_connecte(Utilisateur utilisateur);
+    /// @brief Fonction qui trouve un utilisateur à l'aide de son mail et de son mot de passe.
     Utilisateur recherche_utilisateur(std::string adresse_mail,
                                       std::string mot_de_passe);
 };
