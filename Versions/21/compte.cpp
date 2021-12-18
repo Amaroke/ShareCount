c2d107e3f721f08931c404bdcb748c7dde9d33f9
Amaroke <hugo.mathieu-steinbach5@etu.univ-lorraine.fr>
2021-12-18 11:08:25 +0100

    Gestion des dépenses partagées.

---
+++ Versions/21/compte.cpp
@@ -0,0 +1,29 @@
+/* compte.cpp
+ * version 7
+ * L. Yvoz, univ. Lorraine
+ */
+
+#include "compte.hpp"
+
+// Le constructeur permet de définir les champs
+// de la classe en fonction des paramètres passés.
+//
+// createur l'utilisateur ayant créé le compte.
+// nom le nom du compte.
+// type le type du compte représenté par un entier (1 : compte de dépenses partagées, 2 : cagnotte, 3 : compte commun).
+Compte::Compte(const Utilisateur& createur, const std::string& nom, const int type)
+    : createur(createur)
+    , nom(nom)
+    , type(type)
+{
+}
+
+// Le destructeur ne fait rien.
+Compte::~Compte() { }
+
+// Fonction qui retourne un entier correspondant au type du compte
+//
+// le type du compte
+int Compte::getType() {
+    return type;
+}
